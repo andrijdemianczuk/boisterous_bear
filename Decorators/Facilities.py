4c71f0d73176d92cc4838435e8e71534d9d3e000
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2022-12-07 09:36:36 -0700

    Added singleton decorator and applied to the publisher class

---
+++ Decorators/Facilities.py
@@ -0,0 +1,14 @@
+import functools
+
+
+def Singleton(cls):
+    """Make a class a Singleton"""
+
+    @functools.wraps(cls)
+    def wrapper_singleton(*args, **kwargs):
+        if not wrapper_singleton.instance:
+            wrapper_singleton.instance = cls(*args, **kwargs)
+        return wrapper_singleton.instance
+
+    wrapper_singleton.instance = None
+    return wrapper_singleton
--- Helpers/Publish.py
+++ Helpers/Publish.py
@@ -1,24 +1,30 @@
 from kafka import KafkaProducer
+from Decorators import Facilities
 
 
-def publish_message(producer_instance, topic_name, key, value):
-    try:
-        key_bytes = bytes(key, encoding='utf-8')
-        value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
-        producer_instance.flush()
-        print('Message published successfully.')
-    except Exception as ex:
-        print('Exception in publishing message')
-        print(str(ex))
+@Facilities.Singleton
+class Publisher:
 
+    def __init__(self):
+        self._producer = None
 
-def connect_kafka_producer() -> KafkaProducer:
-    _producer = None
-    try:
-        _producer = KafkaProducer(bootstrap_servers=['35.86.112.176:9092'], api_version=(0, 10))
-    except Exception as ex:
-        print('Exception while connecting Kafka')
-        print(str(ex))
-    finally:
-        return _producer
+    def publish_message(self, topic_name, key, value):
+        try:
+            key_bytes = bytes(key, encoding='utf-8')
+            value_bytes = bytes(value, encoding='utf-8')
+            self.send(topic_name, key=key_bytes, value=value_bytes)
+            self.flush()
+            print('Message published successfully.')
+        except Exception as ex:
+            print('Exception in publishing message')
+            print(str(ex))
+
+    def connect_kafka_producer(self) -> KafkaProducer:
+        _producer = None
+        try:
+            self._producer = KafkaProducer(bootstrap_servers=['35.86.112.176:9092'], api_version=(0, 10))
+        except Exception as ex:
+            print('Exception while connecting Kafka')
+            print(str(ex))
+        finally:
+            return self._producer
