07f9f473b7381b01eaf1174ff3a31fc5a154f72c
andrijdemianczuk <andrijdemianczuk>
2023-01-05 23:07:36 +0000

    Updated Surface Map notebook to include a 2d representation

--- Notebooks/Production/Streaming Live Tables (DLT)/DTS-Render-Surface-Map.py
+++ Notebooks/Production/Streaming Live Tables (DLT)/DTS-Render-Surface-Map.py
@@ -21,6 +21,7 @@
 # COMMAND ----------
 
 from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
 
 # COMMAND ----------
 
@@ -36,10 +37,45 @@ pdf = df.sort(F.col("timestamp").desc()).limit(60).drop("timestamp").toPandas()
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ## Reviewing the original data source
+# MAGIC 
+# MAGIC Let's quickly review the sample DTS temperature chart that plots temp readings across the span according to line depth:
+# MAGIC 
+# MAGIC <img src="/files/Users/andrij.demianczuk@databricks.com/resources/Screenshot_2022_12_12_at_1_47_25_PM.png" width=750/>
+# MAGIC 
+# MAGIC Based on this visual representation, we segmented the above chart into logical parts as an approximation of real data. Clearly our simulated data is less variable, but for the sake of demonstration we are going to assume a similar shape so we can extend our persective by time as a third dimension:
+# MAGIC 
+# MAGIC <img src="/files/Users/andrij.demianczuk@databricks.com/resources/Screenshot_2022_12_12___segmented.jpg" width=750 />
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Taking a singular point-in-time representation of the demo data
+# MAGIC 
+# MAGIC Before we add the time dimension to the data, we will do a quick render based on the melted data to preview what our line graph looks like with the simulated data.
+
+# COMMAND ----------
+
+samp = spark.table("hive_metastore.ademianczuk.ad_dlt_dts_melted")
+
+samp = samp.withColumn("timestamp", F.unix_timestamp(F.col("timestamp")).alias("timestamp"))
+samp = samp.filter(F.col("timestamp") == 1672863962)
+samp = samp.withColumn("segment", F.regexp_replace("segment", "seg_", ""))
+samp = samp.withColumn("segment", F.col("segment").cast(IntegerType()))
+samp = samp.orderBy(F.col("segment").asc())
+
+# COMMAND ----------
+
+display(samp)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Presenting the data
 # MAGIC 
 # MAGIC <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Plotly-logo.png/1200px-Plotly-logo.png" width=300 />
 # MAGIC 
-# MAGIC ## Presenting the data
 # MAGIC 
 # MAGIC Since we removed a lot of the logic used to clean, munge and organize our data out to the DLT pipeline we now just need to focus on the presentation of the data. Since we are concerned with analyzing the data across three dimensions (segment length, temperature and time) we can easily transform the data to a vector that can be rendered by the plotly library.
 # MAGIC 
@@ -54,9 +90,9 @@ fig.update_layout(title='DTS 10-hr History - Well 01-01P',
                   margin=dict(l=0, r=0, b=0, t=50), 
                   width=1000, height=750,
                  scene=dict(
-                            xaxis_title='Segment',
+                            xaxis_title='Distance - Meters',
                             yaxis_title='Hour',
-                            zaxis_title='Temperature',
+                            zaxis_title='Temperature - Deg C',
                             ))
 
 fig.show()
